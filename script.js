cbebc070d6a0ce58d8039b0e798d317ea03a6207
Rayman45 <thepytales@gmail.com>
2026-04-02 15:00:44 +0200

    s

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Grabber-Update: Findet jetzt zuverlässig übergeordnete Gruppen (Spinner/Sanduhr)
+// Grabber-Update: Nutzt closest() für garantierte Greifbarkeit von Gruppen
 AFRAME.registerComponent('grabber', {
     init: function () {
         this.onTriggerDown = this.onTriggerDown.bind(this);
@@ -11,17 +11,15 @@ AFRAME.registerComponent('grabber', {
         const raycaster = this.el.components.raycaster;
         if (!raycaster || raycaster.intersections.length === 0) return;
         
-        let target = raycaster.intersections[0].object.el;
-        // Suche hoch bis zur Szene nach der .grabbable Klasse
-        while (target && target.tagName !== 'A-SCENE' && !target.classList.contains('grabbable')) {
-            target = target.parentElement;
-        }
+        let intersectEl = raycaster.intersections[0].object.el;
+        // FIND-FIX: closest() findet zuverlässig die .grabbable Eltern-Entity, auch in Gruppen
+        let target = intersectEl.closest('.grabbable');
         
-        if (target && target.classList.contains('grabbable')) {
+        if (target) {
             this.grabbedEntity = target;
             this.el.object3D.attach(this.grabbedEntity.object3D);
-            // Event für Spezialeffekte (wie Squish) feuern
-            this.grabbedEntity.emit('grabbed', { hand: this.el });
+            // Sagt dem Ball, welche Hand ihn gegriffen hat
+            this.grabbedEntity.emit('grabbed', { handEl: this.el });
         }
     },
     onTriggerUp: function (evt) {
@@ -30,6 +28,77 @@ AFRAME.registerComponent('grabber', {
             this.el.sceneEl.object3D.attach(this.grabbedEntity.object3D);
             this.grabbedEntity = null;
         }
+    }
+});
+
+// Fidget Spinner Logik
+AFRAME.registerComponent('spinable', {
+    init: function () {
+        this.spinVelocity = 0;
+        this.friction = 0.985;
+        this.el.addEventListener('click', () => { this.spinVelocity += 20; });
+    },
+    tick: function (time, timeDelta) {
+        this.el.object3D.rotation.y += this.spinVelocity * (timeDelta / 1000);
+        this.spinVelocity *= this.friction;
+    }
+});
+
+// Stress-Ball Logik: Quetscht realistisch gegen die Handfläche
+AFRAME.registerComponent('squishable', {
+    init: function () {
+        this.isSqueezed = false;
+        this.handRotation = new THREE.Euler();
+        
+        this.el.addEventListener('grabbed', (evt) => { 
+            this.isSqueezed = true; 
+            // Kopiert die aktuelle Hand-Rotation, um den Ball relativ flach zu quetschen
+            this.handRotation.copy(evt.detail.handEl.object3D.rotation);
+        });
+        
+        this.el.addEventListener('released', () => { 
+            this.isSqueezed = false; 
+        });
+    },
+    tick: function () {
+        const currentScale = this.el.getAttribute('scale') || {x: 1, y: 1, z: 1};
+        let targetScale = {x: 1, y: 1, z: 1};
+
+        if (this.isSqueezed) {
+            // SQUISH-FIX: Wir drehen den Ball lokal gegen die Hand und quetschen die Z-Achse (Tiefe)
+            // Die Hand-Rotation wird übernommen, plus eine kleine Korrektur
+            this.el.object3D.rotation.set(this.handRotation.x, this.handRotation.y, this.handRotation.z + Math.PI / 2);
+            // Wir quetschen flach (Z) und verbreitern (X, Y)
+            targetScale = {x: 1.3, y: 1.3, z: 0.5}; 
+        }
+
+        this.el.setAttribute('scale', {
+            x: currentScale.x + (targetScale.x - currentScale.x) * 0.15,
+            y: currentScale.y + (targetScale.y - currentScale.y) * 0.15,
+            z: currentScale.z + (targetScale.z - currentScale.z) * 0.15
+        });
+    }
+});
+
+// Sanduhr Logik
+AFRAME.registerComponent('sand-timer', {
+    init: function () {
+        this.sandLevel = 1.0; 
+        this.sandEl = this.el.querySelector('#sand');
+    },
+    tick: function (time, timeDelta) {
+        const rotation = this.el.object3D.rotation;
+        const isUpsideDown = Math.abs(rotation.x) > Math.PI / 2 || Math.abs(rotation.z) > Math.PI / 2;
+        
+        if (isUpsideDown && this.sandLevel > 0) {
+            this.sandLevel -= 0.001 * (timeDelta / 16); 
+        } else if (!isUpsideDown && this.sandLevel < 1.0) {
+             this.sandLevel += 0.001 * (timeDelta / 16); 
+        }
+
+        this.sandEl.setAttribute('scale', {x: 1, y: this.sandLevel, z: 1});
+        const offset = (1 - this.sandLevel) * 0.09;
+        this.sandEl.setAttribute('position', {x: 0, y: isUpsideDown ? offset : -offset, z: 0});
     }
 });
 
